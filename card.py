b95c298fb7267d897aebf92ba29a59193c2c79f6
Pierre Baume <pierre@netnod.se>
2025-06-14 10:33:23 +0200

    Start card.py

---
+++ card.py
@@ -0,0 +1,8 @@
+# Combine all solvers in solver_evo into a single one
+
+# Outline:
+# 1. Read all source files in the solver_evo directory
+# 2. Rename all variables to avoid conflicts
+# 3. Replace duplicate calls with corresponding variables
+# 4. Output a single file, batt.py
+
