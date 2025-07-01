c261fb8abed6fed4ac032b5e4a5d8a3441f6b2a1
Pierre Baume <pierre@baume.org>
2025-07-01 20:31:47 +0200

    Refine solver_dir

--- clean_def.sh
+++ clean_def.sh
@@ -22,7 +22,7 @@ fi
 # Change to your target directory
 cd $SOLVER_DIR || exit 1
 
-ls */* | shuf -n "$MAX_NUM_FILES" || {
+ls 0-9]*/[0-9a-f]* | shuf -n "$MAX_NUM_FILES" || {
     echo "Error: Failed to list or shuffle files."
     exit 1
 }
