84b0c1e33c9540e61dca84c4e82000b1c3876072
Pierre Baume <pierre@baume.org>
2025-07-01 20:28:12 +0200

    Refine solver_dir

--- count_solvers.sh
+++ count_solvers.sh
@@ -1,9 +1,9 @@
   SOLVER_DIR=$1
 
   echo -n Total solvers:
-  ls $SOLVER_DIR/*/*.def | wc -l
+  ls $SOLVER_DIR/*/*/*.def | wc -l
   echo -n Known solvers:
-  for f in `ls $SOLVER_DIR/*/*.def`; do 
+  for f in `ls $SOLVER_DIR/*/*/*.def`; do 
     for n in `echo $f | grep -o solve_........`; do 
       grep $n solvers.py
     done
