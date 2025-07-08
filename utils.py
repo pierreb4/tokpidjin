ebb9c304d482b98c62fb11e88b9286bcdfa60581
Pierre Baume <pierre@baume.org>
2025-07-08 11:34:25 +0200

    Refine solver_dir

--- utils.py
+++ utils.py
@@ -135,14 +135,12 @@ def get_source(task_id, imports=None, best_only=False):
             if not mod_item:
                 continue
 
-            # try:
             solver_module = load_path(mod_item['path'])
             if solver_module is None:
                 continue
+
             func_name = mod_item['name']
             solver = getattr(solver_module, func_name)
-            # except (AttributeError, FileNotFoundError):
-            #     continue
             print_l(f'Found dir solver: {func_name} in {solver_module.__name__}')
             return func_name, inspect.getsource(solver)
 
