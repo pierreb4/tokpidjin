194b97f88ba809bcf2136f67dbb5a82da080de73
Pierre Baume <pierre@baume.org>
2025-07-31 13:06:56 +0200

    Refine solver_dir

--- run_batt.py
+++ run_batt.py
@@ -249,7 +249,10 @@ def run_batt(total_data, task_i, task_id, start_time, fluff_log_path, timeout=1)
         task_s_score = s_score.get(task_id, 0)
 
         # solver_score = f'solver_dir/solve_{task_id}/{o_score[sol_t]}/{task_s_score}/{t_log[sol_t]}'
-        solver_score = f'solver_dir/solve_{sol_tid}/{task_o_score}/{task_s_score}/{t_log}'
+        # solver_score = f'solver_dir/solve_{sol_tid}/{task_o_score}/{task_s_score}/{t_log}'
+
+        # Prioritise t_log over task_s_score
+        solver_score = f'solver_dir/solve_{sol_tid}/{task_o_score}/{t_log}/{task_s_score}'
 
         # print_l(f'-> {solver_score}/{md5_hash}.py')
 
