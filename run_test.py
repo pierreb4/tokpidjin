1c0b8ca178ce5535d9b334f21b1aeff843c5150f
Pierre Baume <pierre@baume.org>
2025-08-22 17:06:52 +0200

    Refine solver_dir

--- run_test.py
+++ run_test.py
@@ -199,11 +199,6 @@ def check_solvers_correctness(data, solvers_module, specific_id=None, quiet=Fals
             print(f"Task_id '{task_id}' not found in training data.")
             return
 
-        solver_source = get_solver_source(task_id, imports=None, best_only=True)
-        module_name = solver_source.path[:-3].replace('/', '.')
-        solver_module = importlib.import_module(module_name)
-        solver = solver_module.solve
-
     else:
         task_ids = data['train'].keys()
         for task_id in task_ids:
@@ -225,9 +220,7 @@ def check_solvers_correctness(data, solvers_module, specific_id=None, quiet=Fals
         num_train = len(data['train'][task_id])
         num_test = len(data['test'][task_id])
 
-
         solver_source = get_solver_source(task_id, imports=None, best_only=True)
-
         if solver_source.path is None:
             # print_l(f"No solver found for {task_id}, skipping...")
             continue
@@ -235,20 +228,10 @@ def check_solvers_correctness(data, solvers_module, specific_id=None, quiet=Fals
         module_name = solver_source.path[:-3].replace('/', '.')
         solver_module = importlib.import_module(module_name)
         solver = solver_module.solve
-
-        print(f"Loaded solver for {task_id} from {solver_source.path}")
-
+        # print(f"Loaded solver for {task_id} from {solver_source.path}")
 
         S = tuple((tuple(sample['input']), tuple(sample['output'])) for sample in task)
         try:
-            # if solver is not None:
-            #     pass
-            # elif task_id in solve_func and hasattr(solvers_module, solve_func[task_id]):
-            #     solver = getattr(solvers_module, solve_func[task_id])
-            # if task_id in solve_func: 
-            # else:
-            #     continue
-
             correct = 1
             for i, ex in enumerate(task):
                 k_type = 'Train' if i < num_train else 'Test'
