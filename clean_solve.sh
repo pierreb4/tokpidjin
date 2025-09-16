188edb6daa52be9652a8918e160199c6391a14d2
Pierre Baume <pierre@baume.org>
2025-09-16 04:52:37 +0200

    Refine solver_dir

--- clean_solve.sh
+++ clean_solve.sh
@@ -10,7 +10,7 @@ SOLVER_DIR="$HOME/dsl/tokpidjin/$1"
 MAX_NUM_FILES="$2"
 
 if [ -z "$MAX_NUM_FILES" ]; then
-    MAX_NUM_FILES=20
+    MAX_NUM_FILES=50
 fi
 
 # Check if the provided argument is a valid directory
