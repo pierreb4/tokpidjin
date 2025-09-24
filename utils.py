a6a8fc3d44a30f76e3694af0cdad02659493fc8b
Pierre Baume <pierre@baume.org>
2025-09-24 16:59:30 +0200

    Refine solver_dir

--- utils.py
+++ utils.py
@@ -304,8 +304,8 @@ def get_solver_source(task_id, imports=None, best_only=False):
             file_paths = glob.glob(f'solver_dir/solve_{task_id}/[0-9]*/[0-9]*/[0-9a-f]*.py')
             if not file_paths:
                 # print_l(f'No solver found for {task_id}, using identity')
-                return Solver('solve', None, None, 0, 0)
-                # return Solver('solve', None, solve_identity, 0, 999)
+                # return Solver('solve', None, None, 0, 0)
+                return Solver('solve', None, solve_identity, 0, 0)
             random.shuffle(file_paths)
             for file_path in file_paths:
                 sections = file_path.split('/')
@@ -353,7 +353,7 @@ def get_solver_source(task_id, imports=None, best_only=False):
 
     # print_l(f'No solver found for {task_id}, using None')
     return Solver('solve', None, None, 0, 0)
-    # return Solver('solve', None, solve_identity, 0, 999)
+    # return Solver('solve', None, solve_identity, 0, 0)
 
 
 def get_differs(import_names, best_only=False):
