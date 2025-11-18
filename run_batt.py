58a3698370ba16a296fc5bc9f467d44cc5d1cb7a
Pierre Baume <pierre@baume.org>
2025-11-18 11:33:34 +0100

    Refine run_batt.py

--- run_batt.py
+++ run_batt.py
@@ -1201,7 +1201,7 @@ def check_solver_save(path, score, max_files=32):
     return no_save
 
 
-# XXX Quick and dirty workaround to promote differ diversity
+# XXX Quick and dirty workaround to promote diversity
 def check_differ_save(path, score, max_files=32):
     # List subpaths in path
     root_path = Path(path)
